2a509db50e345a9e6faad54e7164796ad17b51c1
Isaac Winner <149059161+IsaacWinner18@users.noreply.github.com>
2024-02-29 15:17:11 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const checkCre = () => {
 
 const username = document.querySelector('#username').value;
-const password = document.querySelector('#password').value;
-const mssg = document.querySelector('#mssg')
+const password = document.querySelector("#password").value;
+const mssg = document.querySelector('#mssg');
 
 if (username === 'username' && password === 'password') {
-    window.location.href= 'second.html'
+    window.location.href= 'second.html';
+    mssg.style.display = 'none';
 }
 else {
     mssg.style.display = 'block';
+    
 }
 
 }
